bd5490bfe2c4002fa7cb7c832f433e4e0f05567a
juyeong-repo <juyeong.park.tech@gmail.com>
2023-02-18 15:46:32 +0900

    feat: user model 생성

--- src/main/java/com/juyeong/photogram/web/AuthController.java
+++ src/main/java/com/juyeong/photogram/web/AuthController.java
@@ -1,12 +1,20 @@
 package com.juyeong.photogram.web;
 
+import com.juyeong.photogram.web.dto.auth.SignupReqDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+
+
 //인증
 @Controller
 public class AuthController {
+
+    private static Logger log = LoggerFactory.getLogger(AuthController.class);
+
     @GetMapping("/auth/signup")
     public String signupForm () {
         return "auth/signup";
@@ -19,10 +27,12 @@ public class AuthController {
 
     // 회원가입 완료하면 로그인 화면으로 가세요
     @PostMapping("/auth/signup")
-    public String signup () {
-        //csrf 토큰이 활성화되어 signin으로 이동못함
+    public String signup (SignupReqDto signupReqDto) {
+        log.info(signupReqDto.toString());
         return "auth/signin";
     }
+
+    //서버 리로드 안되잔아 ,,,,,
 
 }
 
---
+++ src/main/java/com/juyeong/photogram/web/dto/auth/SignupReqDto.java
@@ -0,0 +1,14 @@
+package com.juyeong.photogram.web.dto.auth;
+
+import lombok.Data;
+
+@Data
+public class SignupReqDto {
+    private String username;
+    private String password;
+    private String email;
+    private String name;
+
+    //role(필수), gender(선택) 등 추가정보는 추후 추가
+
+}
