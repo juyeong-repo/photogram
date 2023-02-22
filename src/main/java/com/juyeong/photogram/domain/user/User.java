caaf38a2b765217e19a8a781758024326ab4816e
juyeong-repo <juyeong.park.tech@gmail.com>
2023-02-22 15:39:30 +0900

    feat : 사용자 username 중복 방지 - 유니크 키로 설정

--- src/main/java/com/juyeong/photogram/domain/user/User.java
+++ src/main/java/com/juyeong/photogram/domain/user/User.java
@@ -18,6 +18,7 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) //AI, sequence
     private int id;
+    @Column(unique = true)
     private String username;
     private String password;
     private String name;
