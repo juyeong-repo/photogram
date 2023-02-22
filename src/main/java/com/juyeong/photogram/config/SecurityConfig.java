7217209768190d279e547bc04dd64a115b5b107b
juyeong-repo <juyeong.park.tech@gmail.com>
2023-02-22 15:37:57 +0900

    feat : 회원 비밀번호 암호화 (BCryptPasswordEncoder 사용) 하여 DB에 저장

--- src/main/java/com/juyeong/photogram/config/SecurityConfig.java
+++ src/main/java/com/juyeong/photogram/config/SecurityConfig.java
@@ -1,13 +1,21 @@
 package com.juyeong.photogram.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity //이 파일로 시큐리티 활성화
 @Configuration //메모리에 띄우자 .ioc
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public BCryptPasswordEncoder encode() {
+        // IoC 할 때 빈이 주입되도록 객체를 heap에 생성
+        return new BCryptPasswordEncoder();
+    }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         //super 삭제하면 기존 시큐리티가 가지고 있던 기능이 삭제됨
--- src/main/java/com/juyeong/photogram/service/AuthService.java
+++ src/main/java/com/juyeong/photogram/service/AuthService.java
@@ -3,14 +3,23 @@ package com.juyeong.photogram.service;
 import com.juyeong.photogram.domain.user.User;
 import com.juyeong.photogram.domain.user.UserRepositoty;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service // IoC, 트랜잭션 관리
 @RequiredArgsConstructor
 public class AuthService {
 
     private final UserRepositoty userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    @Transactional
     public User signUp(User user) {
+        String rawPassword = user.getPassword();
+        String encPassword = bCryptPasswordEncoder.encode(rawPassword);
+        user.setPassword(encPassword);
+        user.setRole("ROLE_USER"); // 관리자 ROLE_ADMINß
+
         // 회원가입
         User userEntity = userRepository.save(user);
         return userEntity;
