feb6b7bcb28f00e9702c6cd7f4275a27e526f221
juyeong-repo <juyeong.park.tech@gmail.com>
2023-02-28 17:30:45 +0900

    refactor : 유효성 검사 에러 script로 던지기

--- src/main/java/com/juyeong/photogram/handler/ControllerExceptionHandler.java
+++ src/main/java/com/juyeong/photogram/handler/ControllerExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.juyeong.photogram.handler;
 
 import com.juyeong.photogram.handler.ex.CustomValidationException;
+import com.juyeong.photogram.util.Script;
 import com.juyeong.photogram.web.dto.CMRespDto;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,9 +14,17 @@ import java.util.Map;
 public class ControllerExceptionHandler {
 
     @ExceptionHandler(CustomValidationException.class)
-    public CMRespDto<?> validationException(CustomValidationException e) {
-            //리턴타입에 제네릭 추론을 사용하여 <?> 라고만 적어도 ok
-        return new CMRespDto<Map<String, String>>(-1, e.getMessage(), e.getErrMap());
+    public String validationException(CustomValidationException e) {
+       /* CMRespDto와 Script 비교
+       * 1. 클라이언트(브라우저)에게 보여줄 때는 스크립트가 좋음
+       * 2. 에이잭스로 통신하거나 안드로이드 통신 시 (개발자가 응답받음)에는 custrom response dto 가 좋음
+       * 둘 중 어떤 방식이 나은지는 상황에 따라 판별하자
+       * */
+
+
+    return Script.back(e.getErrMap().toString());
+
+        //return new CMRespDto<Map<String, String>>(-1, e.getMessage(), e.getErrMap());
     }
 
 }
