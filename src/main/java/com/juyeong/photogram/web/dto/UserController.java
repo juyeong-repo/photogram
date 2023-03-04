b98c8d52b1f9db50929f298c9e8b379f3f58f5b8
juyeong-repo <juyeong.park.tech@gmail.com>
2023-03-04 21:53:15 +0900

    feat : 유저 세션 정보 확인

--- src/main/java/com/juyeong/photogram/web/dto/UserController.java
+++ src/main/java/com/juyeong/photogram/web/dto/UserController.java
@@ -1,6 +1,10 @@
 package com.juyeong.photogram.web.dto;
 
 
+import com.juyeong.photogram.config.auth.PrincipalDetails;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,7 +18,13 @@ public class UserController {
     }
 
     @GetMapping("/user/{id}/update")
-    public String update(@PathVariable int id) {
+    public String update(@PathVariable int id, @AuthenticationPrincipal PrincipalDetails principalDetails) {
+        System.out.println("session info using annotation:::::::" + principalDetails.getUser());
+
+        //직접 세션정보 찾아들어가서 꺼내오기 (추후 주석)
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        PrincipalDetails mPrincipalDetails =  (PrincipalDetails) auth.getPrincipal();
+        System.out.println("session info from Security Context Holder ::::::::::" + mPrincipalDetails.getUser());
         return "user/update";
     }
 
