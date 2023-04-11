c2bc5be566e9448f250c3b106239d7847a246ed2
juyeong-repo <juyeong.park.tech@gmail.com>
2023-04-11 21:28:46 +0900

    feat : User, Image 객체 간 양방향 매핑 - lazy 전략

--- src/main/java/com/juyeong/photogram/domain/user/User.java
+++ src/main/java/com/juyeong/photogram/domain/user/User.java
@@ -1,5 +1,6 @@
 package com.juyeong.photogram.domain.user;
 
+import com.juyeong.photogram.domain.image.Image;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -7,6 +8,7 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Builder
 @Entity
@@ -31,6 +33,14 @@ public class User {
 
     private String profileImageUrl;
     private String role;
+
+    //mappedBy 연관관계의 주인이 아니다. 그러니 테이블에 컬럼 만들지말라고 말해주는 것.
+    //유저 셀릭트 할 때 해당 유저아이디로 등록된 이미지 모두를 가져오라는 뜻
+    // fetch = FetchType.LAZY가 디폴트. 유저를 셀렉트할 때 해당 유저아이디로 등록된 image 갸져오지마 -> getImages 함수의 이미지들이 호출될 때 가져와
+    // eager 전략은 유저를 셀렉트할 때 userID로 등록된 image뜰을 전부 join 해서 가져오기
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+    private List<Image> images;
+
     private LocalDateTime createDate;
 
     @PrePersist 
