eec2f02505cb95f9f548dbfeef795dd7fb3c0288
juyeong-repo <juyeong.park.tech@gmail.com>
2023-04-11 21:29:26 +0900

    feat : 이미지 업로드 시 예외 호출 - 메시지 내림

--- src/main/java/com/juyeong/photogram/web/ImageController.java
+++ src/main/java/com/juyeong/photogram/web/ImageController.java
@@ -1,6 +1,8 @@
 package com.juyeong.photogram.web;
 
 import com.juyeong.photogram.config.auth.PrincipalDetails;
+import com.juyeong.photogram.handler.ex.CustomValidationApiException;
+import com.juyeong.photogram.handler.ex.CustomValidationException;
 import com.juyeong.photogram.service.ImageService;
 import com.juyeong.photogram.web.dto.image.ImageUploadDto;
 import lombok.RequiredArgsConstructor;
@@ -37,6 +39,11 @@ public class ImageController {
         //어떤 정보를 받아야할까 -> 파일을 받아야하므로 -> dto 만들자
         //로그인되어있는 유저 정보가 있어야 한다 -> principalDetails
         //서비스 호출 -> 리턴하면 어디로 보내야하나? 업로드 .. 후에 자기 피드메인으로 보내자
+
+        if (imageUploadDto.getFile().isEmpty()) {
+            throw new CustomValidationException("이미지가 첨부되지 않았습니다.", null);
+        }
+
 
         imageService.uploadFile(imageUploadDto, principalDetails);
         return "redirect:/user/" + principalDetails.getUser().getId();
